1e27df0668c072f7df185221d78007018bf27990
Ben Ivey <benjaminivey00@gmail.com>
2021-01-10 19:17:01 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/aqueous-earth',
+  // "mongodb://localhost/budget",
+   {
   useNewUrlParser: true,
   useFindAndModify: false
 });
